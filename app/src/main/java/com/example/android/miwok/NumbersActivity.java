175a82115dbd7df5df6fc2c3c1defae337bc945e
Anahi Costa <moanableu@gmail.com>
2018-05-06 21:51:09 -0400

    for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +30,46 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numbers = new ArrayList<String>();
+        numbers.add("one");
+        numbers.add("two");
+        numbers.add("three");
+        numbers.add("four");
+        numbers.add("five");
+        numbers.add("six");
+        numbers.add("seven");
+        numbers.add("eight");
+        numbers.add("nine");
+        numbers.add("ten");
+
+       //Log.v("NumbersActivity", "numbers array at index 0 " + numbers.get(0));
+
+        //dynamically create view:
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        int index;
+
+/*      int index = 0;
+
+            while (index < numbers.size()){
+            //Log.v("NumbersActivity", "Index" + index + "Value: "+ numbers.get(index));
+            //create new text view, important: context!
+            TextView numbersView = new TextView(this);
+
+            // set the text:
+            numbersView.setText(numbers.get(index));
+
+            //add view to parent layout
+            rootView.addView(numbersView);
+
+            index++;
+        }*/
+
+        for (index = 0; index < numbers.size(); index++){
+            TextView numbersView = new TextView(this);
+            numbersView.setText(numbers.get(index));
+            rootView.addView(numbersView);
+        }
     }
 }
