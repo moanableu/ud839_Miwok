6f12a44ff09c4e0cd5bf3c93995de2f1125954ed
Anahi Costa <moanableu@gmail.com>
2018-05-16 22:01:26 -0400

    array adapter added

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -19,10 +19,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static com.example.android.miwok.R.id.rootView;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -43,14 +48,20 @@ public class NumbersActivity extends AppCompatActivity {
         numbers.add("nine");
         numbers.add("ten");
 
-       //Log.v("NumbersActivity", "numbers array at index 0 " + numbers.get(0));
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbers);
 
-        //dynamically create view:
+        ListView listView = findViewById(rootView);
+
+        listView.setAdapter(itemsAdapter);
+
+       /*Log.v("NumbersActivity", "numbers array at index 0 " + numbers.get(0));
+
+        dynamically create view by looping thru a list
         LinearLayout rootView = findViewById(R.id.rootView);
 
         int index;
 
-/*      int index = 0;
+        int index = 0;
 
             while (index < numbers.size()){
             //Log.v("NumbersActivity", "Index" + index + "Value: "+ numbers.get(index));
@@ -64,12 +75,14 @@ public class NumbersActivity extends AppCompatActivity {
             rootView.addView(numbersView);
 
             index++;
-        }*/
-
-        for (index = 0; index < numbers.size(); index++){
+        }
+            for (index = 0; index < numbers.size(); index++){
             TextView numbersView = new TextView(this);
             numbersView.setText(numbers.get(index));
             rootView.addView(numbersView);
-        }
+        }*/
+
+
+
     }
 }
